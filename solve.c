76333ff543ef7a4c139d50fea100ff82da3c63e5
Charles <chkya8@gmail.com>
2023-08-06 12:51:30 +0800

    solve

--- solve.c
+++ solve.c
@@ -13,52 +13,170 @@
 int	is_valid(int **grid, int row, int col, int val);
 int	check_visibility(int *clues[4], int **ans);
 
-int solve_skyscraper(int **ans, int *clues[4], int size)
+int check_double(int tab[4][4], int pos, int num)
 {
-    int row = 0, col = 0;
-    int stack[size * size * 3]; // Stack to store the row, col, and value
-    int top = -1; // Top of the stack
+	int i;
 
-    while (row < size) {
-        if (ans[row][col] == 0) {
-            int val;
+	i = -1;
+	while (++i < pos / 4)
+		if (tab[i][pos % 4] == num)
+			return (1);
+	i = -1;
+	while (++i < pos % 4)
+		if (tab[pos / 4][i] == num)
+			return (1);
+	return (0);
+}
 
-			val = ans[row][col] + 1;
-            while (val <= size) {
-                if (is_valid(ans, row, col, val) && check_visibility(clues, ans)) {
-                    ans[row][col] = val;
-                    break;
-                }
-                val++;
-            }
+int check_col_up(int tab[4][4], int pos, int entry[16])
+{
+	int i;
+	int max;
+	int count;
 
-            if (val > size) {
-                // No valid value found for the current cell, backtrack.
-                ans[row][col] = 0;
+	i = 0;
+	max = 0;
+	count = 0;
+	if (pos / 4 == 3)
+	{
+		while (i < 4)
+		{
+			if (tab[i][pos % 4] > max)
+			{
+				max = tab[i][pos % 4];
+				count++;
+			}
+			i++;
+		}
+		if (entry[pos % 4] != count)
+			return (1);
+	}
+	return (0);
+}
 
-                // Move to the previous cell
-                do {
-                    top -= 2;
-                    col = stack[top + 1];
-                    row = stack[top];
-                } while (ans[row][col] != 0);
+int check_row_right(int tab[4][4], int pos, int entry[16])
+{
+	int i;
+	int max_size;
+	int visible_towers;
 
-                continue;
-            }
+	i = 4;
+	max_size = 0;
+	visible_towers = 0;
+	if (pos % 4 == 3)
+	{
+		while (--i >= 0)
+		{
+			if (tab[pos / 4][i] > max_size)
+			{
+				max_size = tab[pos / 4][i];
+				visible_towers++;
+			}
+		}
+		if (entry[12 + pos / 4] != visible_towers)
+			return (1);
+	}
+	return (0);
+}
 
-            // Store the current row, col, and value on the stack
-            stack[++top] = row;
-            stack[++top] = col;
-            stack[++top] = val;
-        }
+int check_col_down(int tab[4][4], int pos, int entry[16])
+{
+	int i;
+	int max;
+	int count;
 
-        // Move to the next cell
-        col++;
-        if (col == size) {
-            col = 0;
-            row++;
-        }
+	i = 3;
+	max = 0;
+	count = 0;
+	if (pos / 4 == 3)
+	{
+		while (i >= 0)
+		{
+			if (tab[i][pos % 4] > max)
+			{
+				max = tab[i][pos % 4];
+				count++;
+			}
+			i--;
+		}
+		if (entry[4 + pos % 4] != count)
+			return (1);
+	}
+	return (0);
+}
+
+int check_row_left(int tab[4][4], int pos, int entry[16])
+{
+	int i;
+	int max;
+	int count;
+
+	i = 0;
+	max = 0;
+	count = 0;
+	if (pos % 4 == 3)
+	{
+		while (i < 4)
+		{
+			if (tab[pos / 4][i] > max)
+			{
+				max = tab[pos / 4][i];
+				count++;
+			}
+			i++;
+		}
+		if (entry[8 + pos / 4] != count)
+			return (1);
+	}
+	return (0);
+}
+
+int check_case(int tab[4][4], int pos, int entry[16])
+{
+	if (check_row_left(tab, pos, entry) == 1)
+		return (1);
+	if (check_row_right(tab, pos, entry) == 1)
+		return (1);
+	if (check_col_down(tab, pos, entry) == 1)
+		return (1);
+	if (check_col_up(tab, pos, entry) == 1)
+		return (1);
+	return (0);
+}
+
+int solve(int grid[4][4], int view[16], int pos)
+{
+	int size;
+
+	if (pos == 16)
+		return (1);
+	size = 0;
+	while (++size <= 4)
+	{
+		//checking if the number I want to place is correct
+		if (check_double(grid, pos, size) == 0)
+		{
+            grid[pos / 4][pos % 4] = size;
+			//checking if the grid is correct with the new number
+			if (check_case(grid, pos, view) == 0)
+			{
+				//checking for next possibility
+				if (solve(grid, view, pos + 1) == 1)
+					return (1);
+			}
+			else
+				grid[pos / 4][pos % 4] = 0;
+		}
+	}
+	//did not find any solutions
+	return (0);
+}
+
+int    presolve(int grid[4][4], int view[16], int pos)
+{
+    while (grid[pos / 4][pos % 4] == 0)
+    {
+        pos++;
     }
-
-    return (1);
+    return solve(grid, view, pos);
 }
